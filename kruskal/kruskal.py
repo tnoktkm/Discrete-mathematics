462c7c1ddd6da69ebbcedf48d5ce1bf0ca380aab
tnoktkm <peskow.igor@gmail.com>
2021-02-11 23:17:03 +0400

    GLOBAL UPDATE, CHANCE ON OK
    It global commit, but he needs a piece of changing(a little)

--- kruskal/kruskal.py
+++ kruskal/kruskal.py
@@ -1,78 +1,61 @@
 from dsu import DSU
  
+class Kruskal:
+	def __init__ (self, edges_graph = None):
+		if edges_graph is None:
+			self.edges_graph = []
+		else:
+			self.edges_graph = edges_graph
 
+		self.parents = [i for i in range(0, 2*len(self.edges_graph))] #count vertex on graph with n edges(max)	
+		self.headParents = DSU(self.parents)	#the same, but it a object class DSU
+
+	def Work(self):
+		self.headParents = DSU([i for i in range(0, 2*len(self.edges_graph))])
+		lenTree = 0 #length tree now
+		mst = []
+		self.edges_graph.sort(key=lambda w: w[2])	#sort edges
+		for edge in self.edges_graph:	
+			if not self.headParents.Find(int(edge[0])) == self.headParents.Find(int(edge[1])):	#if they not including
+				self.headParents.Unite(edge[0], edge[1])
+				lenTree += edge[2]
+				print("We choose this edge - " + str(edge))
+
+				mst.append(edge)
+		print("Our graph " + str(self.edges_graph))
+		print("His lenght equal = " + str(lenTree))
+
+		return mst
 
 def main():
-	class Kruskal:
-		def __init__ (self, edges_graph = None):
-			if edges_graph is None:
-				self.edges_graph = []
-			self.parents = [0]	
+	ex1 = [(1, 2, 7),		#example
+			(2, 3, 8),
+			(1, 4, 5),
+			(3, 5, 5),
+			(2, 4, 9),
+			(2, 5, 7),
+			(4, 5, 15),
+			(4, 6, 6),
+			(5, 6, 8),
+			(5, 7, 9),
+			(7, 6, 11)]
+
+	ex2 = 	[(1, 2, 9),
+			(1,6,7),
+			(1,5,5),
+			(2,6,2),
+			(2,3,4),
+			(3,6,1),
+			(3,4,2),
+			(4,6,4),
+			(4,5,9),
+			(5,6,3)]
 
 
-		def EnterEdgesKeyboard(self):
-			count_edge = int(input("Enter count Edge - "))
-			#List with indexs from 1 to countVertices and with the same value
-			for i in range(1, count_edge):
-				self.parents.append(i)
-
-			self.headParents = DSU(self.parents)
-
-
-			for i in range(count_edge):
-				first_vertex, second_vertex, weight = input().split()
-				first_vertex = int(first_vertex)
-				second_vertex = int(second_vertex)
-				weight = int(weight)
-
-				self.edges_graph.append((first_vertex, second_vertex, weight))
-
-		def Testing(self):
-			#self.EnterEdges()
-			lenTree = 0 #length now tree
-			self.edges_graph.sort(key=lambda w: w[2])	#sort edges
-			for edge in self.edges_graph:	
-				if not self.headParents.Find(int(edge[0])) == self.headParents.Find(int(edge[1])):	#if they not including
-					self.headParents.Unite(edge[0], edge[1])
-					lenTree += edge[2]
-					print("We choose this edge - " + str(edge))
-			print("We used " + str(self.edges_graph))
-			print("His lenght equal = " + str(lenTree))
-
-
-	def ApplyExample(test = None):		
-		#List with indexs from 0 to countVertices and with the same value
-		test.parents = [i for i in range(0, 8)]
-
-		test.headParents = DSU(test.parents)	#create dsu struct
-
-		test.edges_graph = [(1, 2, 7),		#example
-							(2, 3, 8),
-							(1, 4, 5),
-							(3, 5, 5),
-							(2, 4, 9),
-							(2, 5, 7),
-							(4, 5, 15),
-							(4, 6, 6),
-							(5, 6, 8),
-							(5, 7, 9),
-							(7, 6, 11)]
-
-		#self.edges_graph = [(1, 2, 9),
-							#(1,6,7),
-							#(1,5,5),
-							#(2,6,2),
-							#(2,3,4),
-							#(3,6,1),
-							#(3,4,2),
-							#(4,6,4),
-							#(4,5,9),
-							#(5,6,3)]
-				
-
-	test = Kruskal()
-	ApplyExample(test)
-	test.Testing()
+	test = Kruskal(ex1)
+	test.Work()   #the same output for graph already finding MST(literaly for already graph MST we saw the same MST graph)
+	test.Work()
+	print("Our MST graph = " + str(test.Work()))  #output our MST graph
 
 if __name__ == '__main__':
     main()
